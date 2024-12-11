e276fb369ff486485526a04652bdc85e54a45b13
Sebastiaan la Fleur <sebastiaan@lfse.nl>
2024-12-11 11:35:36 +0100

    Update setuptools-git-versioning, add py.typed to package data and only activate venv on linux when creating a venv.

--- {{cookiecutter.repo_name}}/ci/linux/create_venv.sh
+++ {{cookiecutter.repo_name}}/ci/linux/create_venv.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 python{{cookiecutter.python_version}} -m venv ./.venv
-. .venv/bin/activate
+if [[ "$OSTYPE" != "win32" && "$OSTYPE" != "msys" ]]; then
+  echo "Activating .venv first."
+  . .venv/bin/activate
+fi
 pip3 install pip-tools
